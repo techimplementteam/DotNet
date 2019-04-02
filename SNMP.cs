a5810e3a88ec85f292048af80a2c888330805c6a
techimplementteam <info@techimplement.com>
2019-04-02 16:34:41 +0500

    Add files via upload

---
+++ SNMP.cs
@@ -0,0 +1,309 @@
+    /// <summary>
+    /// This Class is used to Scan network SNMP Devices based on network Community 
+    /// Differnet SNMP versions is used for differnt operations
+    /// SNMP Walk in Bulk
+    /// SNMP walk in step by step 
+    /// </summary>
+    public class SNMPGetBulk
+    {
+        /// <summary>
+        /// SNMP Get Bulk scan results of SNMP Devices based on network Community and IP address
+        /// </summary>
+        /// <param name="OctCommunity"></param>
+        /// <param name="IpAddress"></param>
+        public void SNMPWalkGetBulk(string OctCommunity, string IpAddress)
+        {
+   
+            // SNMP community name
+            OctetString community = new OctetString(OctCommunity);
+
+            // Define agent parameters class
+            AgentParameters param = new AgentParameters(community);
+            // Set SNMP version to 2 (GET-BULK only works with SNMP ver 2 and 3)
+            param.Version = SnmpVersion.Ver2;
+            // Construct the agent address object
+            // IpAddress class is easy to use here because
+            //  it will try to resolve constructor parameter if it doesn't
+            //  parse to an IP address
+            IpAddress agent = new IpAddress(IpAddress);
+
+            // Construct target
+            UdpTarget target = new UdpTarget((IPAddress)agent, 161, 2000, 1);
+
+            // Define Oid that is the root of the MIB
+            //  tree you wish to retrieve
+            Oid rootOid = new Oid("1.3.6.1.2.1.2.2.1.2"); // ifDescr
+
+            // This Oid represents last Oid returned by
+            //  the SNMP agent
+            Oid lastOid = (Oid)rootOid.Clone();
+
+            // Pdu class used for all requests
+            Pdu pdu = new Pdu(PduType.GetBulk);
+
+            // In this example, set NonRepeaters value to 0
+            pdu.NonRepeaters = 0;
+            // MaxRepetitions tells the agent how many Oid/Value pairs to return
+            // in the response.
+            pdu.MaxRepetitions = 5;
+
+            // Loop through results
+            while (lastOid != null)
+            {
+                // When Pdu class is first constructed, RequestId is set to 0
+                // and during encoding id will be set to the random value
+                // for subsequent requests, id will be set to a value that
+                // needs to be incremented to have unique request ids for each
+                // packet
+                if (pdu.RequestId != 0)
+                {
+                    pdu.RequestId += 1;
+                }
+                // Clear Oids from the Pdu class.
+                pdu.VbList.Clear();
+                // Initialize request PDU with the last retrieved Oid
+                pdu.VbList.Add(lastOid);
+                // Make SNMP request
+                SnmpV2Packet result = (SnmpV2Packet)target.Request(pdu, param);
+                // You should catch exceptions in the Request if using in real application.
+
+                // If result is null then agent didn't reply or we couldn't parse the reply.
+                if (result != null)
+                {
+                    // ErrorStatus other then 0 is an error returned by 
+                    // the Agent - see SnmpConstants for error definitions
+                    if (result.Pdu.ErrorStatus != 0)
+                    {
+                        // agent reported an error with the request
+                        Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                            result.Pdu.ErrorStatus,
+                            result.Pdu.ErrorIndex);
+                        lastOid = null;
+                        break;
+                    }
+                    else
+                    {
+                        // Walk through returned variable bindings
+                        foreach (Vb v in result.Pdu.VbList)
+                        {
+                            // Check that retrieved Oid is "child" of the root OID
+                            if (rootOid.IsRootOf(v.Oid))
+                            {
+                                Console.WriteLine("{0} ({1}): {2}",
+                                    v.Oid.ToString(),
+                                    SnmpConstants.GetTypeName(v.Value.Type),
+                                    v.Value.ToString());
+                                if (v.Value.Type == SnmpConstants.SMI_ENDOFMIBVIEW)
+                                    lastOid = null;
+                                else
+                                    lastOid = v.Oid;
+                            }
+                            else
+                            {
+                                // we have reached the end of the requested
+                                // MIB tree. Set lastOid to null and exit loop
+                                lastOid = null;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No response received from SNMP agent.");
+                }
+            }
+            target.Close();
+        }
+        /// <summary>
+        /// SNMP Walk scan of Devices based on network Community and IP address        
+        /// </summary>
+        /// <param name="OctCommunity"></param>
+        /// <param name="IpAddress"></param>
+        public void SNMPWalkGetNext(string OctCommunity, string IpAddress)
+        {
+            // SNMP community name
+            OctetString community = new OctetString(OctCommunity);
+
+            // Define agent parameters class
+            AgentParameters param = new AgentParameters(community);
+            // Set SNMP version to 1
+            param.Version = SnmpVersion.Ver1;
+            // Construct the agent address object
+            // IpAddress class is easy to use here because
+            //  it will try to resolve constructor parameter if it doesn't
+            //  parse to an IP address
+            IpAddress agent = new IpAddress(IpAddress);
+
+            // Construct target
+            UdpTarget target = new UdpTarget((IPAddress)agent, 161, 2000, 1);
+
+            // Define Oid that is the root of the MIB
+            //  tree you wish to retrieve
+            Oid rootOid = new Oid("1.3.6.1.2.1.2.2.1.2"); // ifDescr
+
+            // This Oid represents last Oid returned by
+            //  the SNMP agent
+            Oid lastOid = (Oid)rootOid.Clone();
+
+            // Pdu class used for all requests
+            Pdu pdu = new Pdu(PduType.GetNext);
+
+            // Loop through results
+            while (lastOid != null)
+            {
+                // When Pdu class is first constructed, RequestId is set to a random value
+                // that needs to be incremented on subsequent requests made using the
+                // same instance of the Pdu class.
+                if (pdu.RequestId != 0)
+                {
+                    pdu.RequestId += 1;
+                }
+                // Clear Oids from the Pdu class.
+                pdu.VbList.Clear();
+                // Initialize request PDU with the last retrieved Oid
+                pdu.VbList.Add(lastOid);
+                // Make SNMP request
+                SnmpV1Packet result = (SnmpV1Packet)target.Request(pdu, param);
+                // You should catch exceptions in the Request if using in real application.
+
+                // If result is null then agent didn't reply or we couldn't parse the reply.
+                if (result != null)
+                {
+                    // ErrorStatus other then 0 is an error returned by 
+                    // the Agent - see SnmpConstants for error definitions
+                    if (result.Pdu.ErrorStatus != 0)
+                    {
+                        // agent reported an error with the request
+                        Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                            result.Pdu.ErrorStatus,
+                            result.Pdu.ErrorIndex);
+                        lastOid = null;
+                        break;
+                    }
+                    else
+                    {
+                        // Walk through returned variable bindings
+                        foreach (Vb v in result.Pdu.VbList)
+                        {
+                            // Check that retrieved Oid is "child" of the root OID
+                            if (rootOid.IsRootOf(v.Oid))
+                            {
+                                Console.WriteLine("{0} ({1}): {2}",
+                                    v.Oid.ToString(),
+                                    SnmpConstants.GetTypeName(v.Value.Type),
+                                    v.Value.ToString());
+                                lastOid = v.Oid;
+                            }
+                            else
+                            {
+                                // we have reached the end of the requested
+                                // MIB tree. Set lastOid to null and exit loop
+                                lastOid = null;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No response received from SNMP agent.");
+                }
+            }
+            target.Close();
+        }
+        /// <summary>
+        /// SNMP walk with version  
+        /// </summary>
+        /// <param name="OctCommunity"></param>
+        /// <param name="IpAddress"></param>
+        public void SNMPWalkVersion(string OctCommunity, string IpAddress)
+        {
+            // SNMP community name
+            OctetString community = new OctetString(OctCommunity);
+
+            // Define agent parameters class
+            AgentParameters param = new AgentParameters(community);
+            // Set SNMP version to 1
+            param.Version = SnmpVersion.Ver1;
+            // Construct the agent address object
+            // IpAddress class is easy to use here because
+            //  it will try to resolve constructor parameter if it doesn't
+            //  parse to an IP address
+            IpAddress agent = new IpAddress(IpAddress);
+
+            // Construct target
+            UdpTarget target = new UdpTarget((IPAddress)agent, 161, 2000, 1);
+
+            // Define Oid that is the root of the MIB
+            //  tree you wish to retrieve
+            Oid rootOid = new Oid("1.3.6.1.2.1.2.2.1.2"); // ifDescr
+
+            // This Oid represents last Oid returned by
+            //  the SNMP agent
+            Oid lastOid = (Oid)rootOid.Clone();
+
+            // Pdu class used for all requests
+            Pdu pdu = new Pdu(PduType.GetNext);
+
+            // Loop through results
+            while (lastOid != null)
+            {
+                // When Pdu class is first constructed, RequestId is set to a random value
+                // that needs to be incremented on subsequent requests made using the
+                // same instance of the Pdu class.
+                if (pdu.RequestId != 0)
+                {
+                    pdu.RequestId += 1;
+                }
+                // Clear Oids from the Pdu class.
+                pdu.VbList.Clear();
+                // Initialize request PDU with the last retrieved Oid
+                pdu.VbList.Add(lastOid);
+                // Make SNMP request
+                SnmpV1Packet result = (SnmpV1Packet)target.Request(pdu, param);
+                // You should catch exceptions in the Request if using in real application.
+
+                // If result is null then agent didn't reply or we couldn't parse the reply.
+                if (result != null)
+                {
+                    // ErrorStatus other then 0 is an error returned by 
+                    // the Agent - see SnmpConstants for error definitions
+                    if (result.Pdu.ErrorStatus != 0)
+                    {
+                        // agent reported an error with the request
+                        Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                            result.Pdu.ErrorStatus,
+                            result.Pdu.ErrorIndex);
+                        lastOid = null;
+                        break;
+                    }
+                    else
+                    {
+                        // Walk through returned variable bindings
+                        foreach (Vb v in result.Pdu.VbList)
+                        {
+                            // Check that retrieved Oid is "child" of the root OID
+                            if (rootOid.IsRootOf(v.Oid))
+                            {
+                                Console.WriteLine("{0} ({1}): {2}",
+                                    v.Oid.ToString(),
+                                    SnmpConstants.GetTypeName(v.Value.Type),
+                                    v.Value.ToString());
+                                lastOid = v.Oid;
+                            }
+                            else
+                            {
+                                // we have reached the end of the requested
+                                // MIB tree. Set lastOid to null and exit loop
+                                lastOid = null;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No response received from SNMP agent.");
+                }
+            }
+            target.Close();
+        }
+    }
